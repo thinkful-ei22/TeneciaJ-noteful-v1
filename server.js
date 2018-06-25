222197624c3b328ba09cfcb8bc105093c22332c7
teneciajennings <jenningst82@gmail.com>
2018-06-25 15:14:45 -0500

    checked with postman

--- server.js
+++ server.js
@@ -9,11 +9,21 @@ console.log('Hello Noteful!');
 
 const express = require('express');
 
-const data = require('./db/notes');
+// const data = require('./db/notes');
 
 const app = express();
 
 app.use(express.static('public'))
+
+data.find(item => item.id === Number(id))
+
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+app.get('/api/notes/:id', (req, res) => {
+  res.json(data);
+});
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
