29e534bbe30e5bb409a634f150e39efc0b2f7075
teneciajennings <jenningst82@gmail.com>
2018-06-26 08:42:55 -0500

    completed noteful v-1

--- server.js
+++ server.js
@@ -13,20 +13,29 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'))
-
-data.find(item => item.id === Number(id))
-
-
-app.get('/api/notes', (req, res) => {
-  res.json(data);
-});
-app.get('/api/notes/:id', (req, res) => {
-  res.json(data);
-});
+app.use(express.static('public'));
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+
+app.get('/api/notes', (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  if (searchTerm) {
+    let filteredData = data.filter(item => item.title.includes(searchTerm));
+    res.json(filteredData);
+  } 
+  else {
+    res.json(data);
+
+  }
+});
+app.get('/api/notes/:id', (req, res) => { 
+  const id = (req.params.id);
+  const data = req.params.find(params => params.id === Number(id));
+  res.json(data);
+});
+
+
